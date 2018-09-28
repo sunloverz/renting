a3ffccfde91d588c8de6fe8f0202a0b361cf0d53
sunloverz <sunloverz.kg@gmail.com>
2018-09-28 16:44:59 +0600

    Fix customers route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from './login/login.component';
 
 const routes: Routes = [
-  { path: 'customer/:id', loadChildren: './customer/customer.module#CustomerModule' },
   { path: 'customers', loadChildren: './customers/customers.module#CustomersModule'},
+  { path: 'customers/:id', loadChildren: './customer/customer.module#CustomerModule' },
   { path: 'account', loadChildren: './account/account.module#AccountModule'},
   { path: 'login', component: LoginComponent}
 ];
