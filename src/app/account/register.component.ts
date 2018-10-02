de58d8fac1637d4946d3208cefa22a1b63a43b02
sunloverz <sunloverz.kg@gmail.com>
2018-10-02 16:08:46 +0600

    Use register reactive forms

--- src/app/account/register.component.ts
+++ src/app/account/register.component.ts
@@ -8,20 +8,37 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
+
+
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
   loading = false;
-  user: any = {};
+  submitted = false;
 
   constructor(
     private router: Router,
+    private formBuilder: FormBuilder,
     private authService: AuthenticationService) { }
 
+  get f() { return this.registerForm.controls; }
+
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      email: ['',  [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      passwordConfirmation: ['', [Validators.required]]
+
+    });
   }
 
   register() {
-    console.log(this.user);
-    this.authService.register(this.user)
+    this.submitted = true;
+
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    this.authService.register(this.registerForm.value)
       .subscribe(
         data => {
           // this.alertService.success('Registration successful', true);
