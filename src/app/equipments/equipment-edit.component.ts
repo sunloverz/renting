9831c30b017d2a425fe050d091afcc9ed10f89d2
KENESHBEK SADYKOV <keneshbek.sadykov@gmail.com>
2018-10-05 20:40:06 +0600

    Edit Equipment done

--- src/app/equipments/equipment-edit.component.ts
+++ src/app/equipments/equipment-edit.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AuthenticationService, EquipmentService} from '../core';
+import {Equipment} from '../core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {first} from 'rxjs/operators';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-equipment-edit',
@@ -6,10 +12,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment-edit.component.css']
 })
 export class EquipmentEditComponent implements OnInit {
+  equipment: Equipment;
+  editForm: FormGroup;
+  equipmentId: String;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute,
+    private equipmentService: EquipmentService,
+    private authService: AuthenticationService) {
   }
 
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.equipmentId = params['id']; // --> Name must match wanted parameter
+    });
+    console.log(this.equipmentId);
+
+    this.editForm = this.formBuilder.group({
+      id: [],
+      name: ['', Validators.required],
+      state: ['', Validators.required],
+      serial_number: ['', Validators.required],
+      vendor_code: ['', Validators.required],
+      price_per_hour: ['', Validators.required],
+      price_per_day: ['', Validators.required],
+      price_per_month: ['', Validators.required]
+    });
+    this.equipmentService.getEquipmentById(+this.equipmentId)
+      .subscribe(data => {
+        this.editForm.setValue(data);
+      });
+  }
+
+  onSubmit() {
+    this.equipmentService.updateEquipment(this.editForm.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.router.navigate(['equipments']);
+        },
+        error => {
+          alert(error);
+        });
+  }
 }
