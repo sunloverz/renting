02c15739e79caa68d2a55bf2eae39618c966e8a8
KENESHBEK SADYKOV <keneshbek.sadykov@gmail.com>
2018-10-11 20:53:21 +0600

    Add Rents

--- src/app/rent/rent.component.ts
+++ src/app/rent/rent.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Rent} from '../core/models/rent';
+import {RentService} from '../core/services';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-rent',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rent.component.css']
 })
 export class RentComponent implements OnInit {
-
-  constructor() { }
+  rents: Rent[];
+  constructor(private rentService: RentService,
+              private router: Router) { }
 
   ngOnInit() {
+    this.getRents();
   }
 
+  getRents(): void {
+    this.rentService.getItems()
+      .subscribe( data => {
+        this.rents = data;
+      });
+  }
+
+  deleteRent(rent: Rent): void {
+    this.rentService.delete(rent.id)
+      .subscribe( data => {
+        this.rents = this.rents.filter(c => c !== rent);
+      });
+  }
+
+  editRent(rent: Rent): void {
+    this.router.navigate(['edit-rent']);
+  }
 }
+
+
